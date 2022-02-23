7033fd7cf0efd37190917a1ebf506b18b52600b3
zakaria444 <elismailizakariae@gmail.com>
2022-02-23 14:25:05 +0100

    new modification

--- React_Project/src/compenent/admin/AfficherHotel.js
+++ React_Project/src/compenent/admin/AfficherHotel.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React ,{ useEffect, useState } from 'react'
 import {Link} from"react-router-dom"
+import Axios from 'axios';
+
 // import jwtDecode from "jwt-decode";
 
 
@@ -14,14 +16,70 @@ import {Link} from"react-router-dom"
 //       return null;
 //   }
 // }
+
 function AfficherHotel() {
 
-  
+
+  const [hotels, sethotels] = useState([]);
+
+  useEffect(()=>{
+  const url="http://localhost:5000/api/hotel"
+  Axios.get(url).then((res) => {
+    
+    sethotels(res.data.data) ;
+
+    })})
+    
+
+
+
+
   return (
     <div>
           <Link className="nav-link active" aria-current="page" to="/dashbordadmin">Back</Link>
 
 <h1>Tout Les Hotel</h1>
+<table className="table">
+
+<thead>
+<tr>
+    <th>Name</th>
+    <th>description</th>
+    <th>city</th>
+    <th>country</th>
+    <th>stars</th>
+
+    <th>Action</th>
+    <th>Action</th>
+
+  
+
+  </tr>
+</thead>
+{ hotels.map(ListHotel =>
+
+<tbody key={ListHotel._id}>
+<tr>
+{/* {console.log(ListHotel.city)} */}
+{/* {console.log(ListHotel)} */}
+  <td>  {ListHotel.name}</td>
+  <td>  {ListHotel.description}</td>
+  <td>  {ListHotel.localisation.city}</td>
+  <td>  {ListHotel.localisation.country}</td>
+
+    <td>  {ListHotel.stars}</td>
+    <td> <Link to={'/client/create'}>Update</Link></td>
+    <td> <Link to={'/client/create'}>delete</Link></td>
+
+
+
+
+  </tr>
+
+ </tbody>
+ )}
+</table>
+    
 
     </div>
   )
