479ea476b6a458942b34d97d4e822b6fede3c2d8
zakaria444 <elismailizakariae@gmail.com>
2022-03-09 17:06:59 +0100

    style dashbord ouwner

--- React_Project/src/compenent/ownerDashbord/dashbordowner.js
+++ React_Project/src/compenent/ownerDashbord/dashbordowner.js
@@ -1,71 +1,148 @@
-import React, { useEffect, useState} from 'react'
-import Axios from 'axios';
+import React, { useState } from "react";
+import Axios from "axios";
 import jwtdecode from "jwt-decode";
 
-import {Link} from"react-router-dom"
+import { Link } from "react-router-dom";
 
 function Home() {
-
   const [owner, setowner] = useState([]);
 
+  const jwt = localStorage.getItem("token");
+  const JWT1 = jwtdecode(jwt);
+  // console.log('jwt parse',JWT1.role);
+  const iduser = JWT1.user_id;
+  const url =
+    "http://localhost:5000/api/propreataire/Getallproprietaires/" + iduser;
 
-  const jwt =  localStorage.getItem('token');
-      const JWT1 =jwtdecode(jwt);
-      console.log('jwt parse',JWT1.role);
-      const iduser=JWT1.user_id;
-    const url="http://localhost:5000/api/propreataire/Getallproprietaires/"+iduser;
-    Axios.get(url)
-    .then((res) => {
-      const responsee =res.data.data[0];
-        console.log('info owner',responsee.name);
-          setowner(responsee);
+    if (owner.length == 0) {
+      Axios.get(url).then((res) => {
+        const responsee = res.data.data[0];
+        // console.log('info owner',responsee.name);
+        setowner(responsee);
         // const response =res.data.data[0];
-      })
+      });
+    }
+  
   // useEffect(()=>{
-    
 
   // });
 
-  
   return (
     <div>
-{/* <h1>dashbordowner</h1> */}
-<div className='main-owner'>
-<div className='admin-dashb'>
-<ul className='nav-admin' id='nav-admin' >
- <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/client/create">Edite Profile</Link>
- </li> 
- <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/owner/create">Ajouter Chambre</Link>
- </li> 
-  <li className="nav-item">
-  <Link className="nav-link active" aria-current="page" to="/admin/createhotel">Ajouter Hotel</Link>
-</li> 
-</ul>
+      {/* <h1>dashbordowner</h1> */}
+      <div className="main-owner">
+        <div className="admin-dashb">
+          <h1 className="afficher-hotel">Hotel</h1>
+          <ul className="nav-admin" id="nav-admin">
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/client/create"
+              >
+                Edite Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/owner/create"
+              >
+                Ajouter Chambre
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/admin/createhotel"
+              >
+                Ajouter Hotel
+              </Link>
+            </li>
+          </ul>
 
-<h1 className='afficher-hotel'>Hotel</h1>
-      <ul className='nav-admin' id='nav-admin'>
-       <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/admin/afficherhotel">Afficher Hotel</Link>
-       </li> </ul>
-      
+          <ul className="nav-admin" id="nav-admin">
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/admin/afficherhotel"
+              >
+                Afficher Hotel
+              </Link>
+            </li>{" "}
+          </ul>
+        </div>
+        <div className="information-owner">
+          {}
+          <h1>Information owner</h1>
+
+          <input value={owner._id}></input>
+          <form>
+            <div className="form-complet">
+              <div className="form-input">
+                <div class="form-group ">
+                  <span>Username</span>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder={owner.username}
+                    id="UserName"
+                  ></input>
+                </div>
+                <div class="form-group ">
+                  <span>Email</span>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder={owner.email}
+                    id="UserName"
+                  ></input>
+                </div>
+                <div class="form-group ">
+                  <span>Name</span>
+
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder={owner.name}
+                    id="UserName"
+                  ></input>
+                </div>
+              </div>
+
+              <div className="image-form"></div>
+            </div>
+          </form>
+          {/* <div className='form-complet'>
+      <div className='form-input'>
+      <div class="form-group ">
+      <span>Username</span>
+      <input type="text" class="form-control" placeholder={owner.username} id="UserName"></input>
+     </div>
+     <div class="form-group ">
+       <span>Email</span>
+      <input type="text" class="form-control" placeholder={owner.email}  id="UserName"></input>
+     </div>
+     <div class="form-group ">
+     <span>Name</span>
+
+      <input type="text" class="form-control" placeholder={owner.name} id="UserName"></input>
+     </div>
+    
+     </div>
+    
+    <div className='image-form'>
 
     </div>
-    <div className='information-owner'>
-    {}
-      <h1 >Information owner</h1>
-      <div>
+     
+     </div> */}
+        </div>
       </div>
-      <input value={owner.name} ></input>
-
-
     </div>
-    </div>
-
-    </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
