36c76b72fc95db263337172a084d2942b89cab39
zakaria444 <elismailizakariae@gmail.com>
2022-03-16 12:10:21 +0100

    fix problem

--- React_Project/src/compenent/ownerDashbord/CreateHotelOwner.js
+++ React_Project/src/compenent/ownerDashbord/CreateHotelOwner.js
@@ -234,8 +234,6 @@ function CreateHotelOwner() {
     </section>
   );
 }
-const ImageThumb = ({ image }) => {
-  return <img className="image-url" src={URL.createObjectURL(image)} alt={image.name} />;
-}
+
 
 export default CreateHotelOwner ;
