00465fb864ed42c1197e36910346c567221fd1d1
zakaria444 <elismailizakariae@gmail.com>
2022-03-18 19:47:11 +0100

    new style

--- React_Project/src/compenent/client/Booking.js
+++ React_Project/src/compenent/client/Booking.js
@@ -10,7 +10,7 @@ function Booking() {
 
     Axios.get(url).then((res) => {
       sethotels(res.data.rooms[0]);
-      console.log("set hotels : ", res.data.rooms[0]);
+      console.table("set hotels : ", res.data.rooms[0]);
     });
 
   }, []);
@@ -20,7 +20,18 @@ function Booking() {
   console.log('hotels in state : ' , hotels);
 
   if (hotels === false) {
-    return <h1>Loding .. </h1>
+    return <div> <div className="spinner-border text-success" role="status">
+    <span className="sr-only">Loading...</span>
+   
+  </div>
+  <h1>loading....</h1>
+  
+  </div>
+    
+ 
+
+    
+    
   }
   
   return (
@@ -50,7 +61,20 @@ function Booking() {
     <div className="col-md-6" >
     <h5 className="card-title">{hotels.hotel_id.name}</h5>   
     <h6 className="card-title">{hotels.hotel_id.description}</h6>
- 
+    <h5 className="card-title">{hotels.type} / {hotels.price} DH</h5>   
+    <div className="img-room">
+    {hotels.images.map(ListHotel =>
+      <div className="col-md-6" key={ListHotel._id}>
+      <div className="card">
+        
+
+        <div> <img src={"http://localhost:3000/"+ListHotel.img} alt="" id="rom-img" className="img-responsive image" /> </div>
+       
+
+        </div>
+      </div>
+      
+      )}
     
     
     
@@ -60,20 +84,10 @@ function Booking() {
       
     
    
-    <div className="img-room">
-     
-    {hotels.images.map(ListHotel =>
-      <div className="col-md-3" key={ListHotel._id}>
-      <div className="card">
-        <div> <img src={"http://localhost:3000/"+ListHotel.img} alt="" id="rom-img" className="img-responsive image" /> </div>
-        </div>
-      </div>
-      
-      )}
-      </div>
+ 
   
 </div>
-
+</div>
 </div>
   );
 }
